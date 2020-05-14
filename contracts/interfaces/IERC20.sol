1b885f8cff731bd66fc8bb32521f674ff82083e8
rotciv <victorfage@gmail.com>
2020-05-14 14:44:39 +0200

    clean project

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.5.12;
 
+
 interface IERC20 {
     function transfer(address _to, uint _value) external returns (bool success);
     function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
--- contracts/testUtils/TestRateOracle.sol
+++ contracts/testUtils/TestRateOracle.sol
@@ -1,56 +1,25 @@
 pragma solidity ^0.5.12;
 
 import "../interfaces/RateOracle.sol";
-import "../utils/BytesUtils.sol";
 
 
-contract TestRateOracle is BytesUtils, RateOracle {
+contract TestRateOracle is RateOracle {
     address internal _token;
+    uint256 public RCNequivalent;
 
-    function symbol() external view returns (string memory) {}
-
-    function name() external view returns (string memory) {}
-
-    function decimals() external view returns (uint256) {}
+    event SetEquivalent(uint256 _equivalent);
 
     function setToken(address token) external returns (address) {
         _token = token;
     }
 
-    function token() external view returns (address) {
-        return _token;
-    }
-
-    function currency() external view returns (bytes32) {}
-
-    function maintainer() external view returns (string memory) {}
-
-    function url() external view returns (string memory) {}
-
-    function encodeRate(
-        uint128 _tokens,
-        uint128 _equivalent
-    ) external pure returns (bytes memory) {
-        return abi.encodePacked(_tokens, _equivalent);
-    }
-
-    function readSample(bytes calldata _data) external returns (uint256 tokens, uint256 equivalent) {
-        if (_data.length != 0) {
-            (bytes32 btokens, bytes32 bequivalent) = decode(_data, 16, 16);
-            tokens = uint256(btokens);
-            equivalent = uint256(bequivalent);
-        } else {
-            tokens = 1000000000000000000000000000000000000;
-            equivalent = RCNequivalent;
-        }
-    }
-    // Used by collateral tests
-    uint256 public RCNequivalent;
-
-    event SetEquivalent(uint256 _equivalent);
-
     function setEquivalent(uint256 _equivalent) external {
         RCNequivalent = _equivalent;
         emit SetEquivalent(_equivalent);
+    }
+
+    function readSample(bytes calldata) external returns (uint256 tokens, uint256 equivalent) {
+        tokens = 1000000000000000000000000000000000000;
+        equivalent = RCNequivalent;
     }
 }
